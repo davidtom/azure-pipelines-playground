ddcd5b424c93ecf23e325ba00e5924af7c3c39e2
David Tomczyk <davidjtomczyk@gmail.com>
2019-04-08 22:25:54 -0700

    final change

--- scripts/version.sh
+++ scripts/version.sh
@@ -13,17 +13,15 @@ vMajor=${PACKAGE_ARRAY[0]}
 vMinor=${PACKAGE_ARRAY[1]}
 vPatch=${PACKAGE_ARRAY[2]}
 
-export V_MAJOR=${PACKAGE_ARRAY[0]}
-export V_MINOR=${PACKAGE_ARRAY[1]}
-export V_PATCH=${PACKAGE_ARRAY[2]}
+echo THIS_WONT=show up
 
 echo "##vso[task.setvariable variable=vMajor]$vMajor"
 echo "##vso[task.setvariable variable=vMinor]$vMinor"
-echo "##vso[task.setvariable variable=vPatch]$"Patch
+echo "##vso[task.setvariable variable=vPatch]$vPatch"
 
 printenv
 
 echo vMajor is: ${V_MAJOR}
-echo vMajor is: $V_MAJOR
+echo vPatch is: $vpatch
 echo global: $GLOBAL
 echo build reason is: $BUILD_REASON
