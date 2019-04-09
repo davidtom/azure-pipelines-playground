5dbb674ddcc2f07ca1949698a900f3ae38f42a6f
David Tomczyk <davidjtomczyk@gmail.com>
2019-04-08 22:20:18 -0700

    omfg

--- scripts/version.sh
+++ scripts/version.sh
@@ -9,9 +9,17 @@ PACKAGE_VERSION=$(cat package.json \
 
 IFS=. read -a PACKAGE_ARRAY <<< "$PACKAGE_VERSION"
 
-echo "##vso[task.setvariable variable=vMajor]$V_MAJOR"
-echo "##vso[task.setvariable variable=vMinor]$V_MINOR"
-echo "##vso[task.setvariable variable=vPatch]$V_PATCH"
+vMajor=${PACKAGE_ARRAY[0]}
+vMinor=${PACKAGE_ARRAY[1]}
+vPatch=${PACKAGE_ARRAY[2]}
+
+export V_MAJOR=${PACKAGE_ARRAY[0]}
+export V_MINOR=${PACKAGE_ARRAY[1]}
+export V_PATCH=${PACKAGE_ARRAY[2]}
+
+# echo "##vso[task.setvariable variable=vMajor]$vMajor"
+# echo "##vso[task.setvariable variable=vMinor]$vMinor"
+# echo "##vso[task.setvariable variable=vPatch]$"Patch
 
 printenv
 
