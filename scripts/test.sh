db944a0b81b94b078b92575eb22bf185854a9244
David Tomczyk <davidjtomczyk@gmail.com>
2019-04-08 21:36:09 -0700

    create script

--- scripts/test.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-vmajor=test
-echo $vmajor
---
+++ scripts/version.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+PACKAGE_VERSION=$(cat package.json \
+    | grep version \
+    | head -1 \
+    | awk -F: '{ print $2 }' \
+    | sed 's/[",]//g' \
+    | tr -d '[[:space:]]')
+
+IFS=. read -a PACKAGE_ARRAY <<< "$PACKAGE_VERSION"
+
+vMajor=${PACKAGE_ARRAY[0]}
+vMinor=${PACKAGE_ARRAY[1]}
+vPatch=${PACKAGE_ARRAY[2]}
+
+echo "##vso[task.setvariable variable=vMajor]$vMajor"
+echo "##vso[task.setvariable variable=vMinor]$vMinor"
+echo "##vso[task.setvariable variable=vPatch]$vPatch"
+
+echo $(vMajor)
+echo $(Build.Reason)
